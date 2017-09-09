48ebb3e7b7894546333ecc555c05be3e3a1912ea
Joseph Villacorta <jvillacorta2@gmail.com>
2017-09-09 22:43:52 +0800

    bookaflight

---
+++ bookaflight.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+<title> Cebu Pacific </title>
+<link rel="stylesheet" type="text/css" href="cssofbook.css">
+<style>
+div.snr{
+	font-family:Helvetica;
+	font-size:25px;
+	margin-top:-77px;
+}
+a{
+	text-decoration:none;
+	color:blue;
+}
+</style>
+</head>
+<body>
+<img src="logo.jpg" alt="Cebu pacific logo">
+<div class="snr">
+<p align ="right">
+<a href="">Sign in</a> | <a href="">Register</a></font>
+</div>
+<hr>
+
+<div id="nav">
+<ul>
+  <li><a class="active" href="#home">Book A Flight</a></li>
+  <li><a href="#news">Reservation</a></li>
+  <li><a href="#contact">View Flight</a></li>
+  </ul>
+</div>
+<div id="booking">
+
+<br>
+<br><br><br>
+<div id="from">
+<h1 class="country">From</h1>
+<select class="combo">
+<option value ="Philippines">Philippines</option>
+<option value ="Hong Kong">Hong Kong</option>
+<option value ="Japan">Japan</option>
+<option value ="South Korea">South Korea</option>
+</select>
+</div>
+<div id="to">
+<h1 class="country">To</h1>
+<select class="combo">
+
+<option value ="Philippines">Philippines</option>
+<option value ="Hong Kong">Hong Kong</option>
+<option value ="Japan">Japan</option>
+<option value ="South Korea">South Korea</option>
+</select>
+</div>
+<div id="dept"><h1 class="country">Departure</h1><input type="text" id="departure" value=""></div>
+<div id="ret"><h1 class="country">Return</h1><input type="text" id="return" value=""></div>
+
+
+<div id="age"><h1 class="country">Age</h1></div>
+<div id="adult"><h1 class="country">Adult</h1><input type="text" value=""></div>
+<div id="child"><h1 class="country">Child</h1><input type="text" value=""></div>
+<div id="infant"><h1 class="country">Infant</h1><input type="text" value=""></div>
+
+
+
+<br><br>
+
+<div id="button1"><input type="submit" value="book"></div>
+
+
+</form>
+</div>
+
+
+
+</body>
+</html>
