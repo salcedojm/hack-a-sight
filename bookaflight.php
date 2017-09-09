44cb1db4cad6c152c3deb21528d77c1cbe7d56cc
PatriceAnn <patrice.ann0104@gmail.com>
2017-09-10 02:07:37 +0800

    book a flight

--- bookaflight.php
+++ bookaflight.php
@@ -29,16 +29,35 @@ a{
   <li><a href="#contact">View Flight</a></li>
   </ul>
 </div>
+
+<center><div id="tabs">
+	<table>
+	<span style="font-family: helvetica; font-size: 26px; color: #1B2631">Where would you like to go?</span>
+	<hr>
+		<tr>
+			<td><a href="#" class="tab1">Round-trip</a></td>
+			<td> </td>
+			<td><a href="#" class="tab1">One-way</a></td>
+			<td> </td>
+			<td><a href="#" class="tab1">Multi-city</a></td>
+		</tr>
+
+	</table>
+
+</div></center>
 <div id="booking">
 
 <br>
 <br><br><br>
 <center>
+
+
 <div>
 	<form>
+
 		<table>
 			<tr>
-				<td><label>From: </label></td>
+				<td><label>From: </label></td>	
 				<td><select class="combo">
 					<option value ="Philippines">Philippines</option>
 					<option value ="Hong Kong">Hong Kong</option>
@@ -46,11 +65,9 @@ a{
 					<option value ="South Korea">South Korea
 					</option>
 					</select></td>
-
 					<td><label>Departure:</label></td>
 				<td>
 				<input type="text" class="departure" placeholder="Day" name="txtd" value=""> <input type="text" class="departure" placeholder="Month" name="txtm" value=""> <input type="text" class="departure" placeholder="Year" name="txty" value=""></td>
-
 			</tr>
 
 			<tr>
@@ -61,24 +78,21 @@ a{
 					<option value ="Japan">Japan</option>
 					<option value ="South Korea">South Korea</option>
 					</select></td>
-
 					<td><label>Return:</label></td>
 				<td>
 				<input type="text" class="departure" placeholder="Day" name="txtd1" value=""> <input type="text" class="departure" placeholder="Month" name="txtm1" value=""> <input type="text" class="departure" placeholder="Year" name="txty1" value=""></td>
-
 			</tr>
-
 		</table>
 <br>
 		<table>
-			
-
+		<hr>
+		<span style="font-family: Helvetica; font-size: 20px; color: #2C3E50; font-weight: bold;">AGE</span>
+		<hr>
 			<tr>
 				<td><label>Adult</label></td>
 				<td><label>Child</label></td>
 				<td><label>Infant</label></td>
 			</tr>
-
 			<tr>
 				<td><input type="text" value=""></div></td>
 				<td><input type="text" value=""></div></td>
