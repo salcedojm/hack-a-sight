2c268fc935a5d2d8c6f2ef0ffe71460309b90671
PatriceAnn <patrice.ann0104@gmail.com>
2017-09-09 19:03:44 +0800

    homepage layout

--- homepage.php
+++ homepage.php
@@ -15,7 +15,44 @@ a{
 	text-decoration:none;
 	color:blue;
 }
+#p{
+	background-color: blue;
+}
+
+.button_back{
+	height: 220px;
+	width: 270px;
+	box-shadow: 10px 10px 5px #CACFD2;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: white;
+}
+.styleText{
+	font-size: 30px;
+	font-weight: bold;
+
+}
+
 </style>
+
+<script src="js/jquery-3.2.1.js"></script>
+<script>
+	$(document).ready(function(){
+		$("#book_flight").click(function(){
+			location.assign("webpagedito.php");
+		});
+		$("#reserve").click(function(){
+		location.assign("webpagedito.php");
+		});
+		$("#view_flight").click(function(){
+		location.assign("webpagedito.php");
+		});
+		$("#nothing").click(function(){
+		location.assign("webpagedito.php");
+		});
+	});
+</script>
 </head>
 <body>
 <img src="logo.jpg" alt="Cebu pacific logo">
@@ -30,11 +67,31 @@ a{
 <br>
 <br>
 <br>
-<input type="submit" name="btnBAF" value="Book-a-Flight" width="1000em">
-<input type="submit" name="btnreserve" value="Reservation">
 <br>
-<input type="submit" name="btnvf" value="View Flight">
-<input type="submit" name="btnewan" value="Book-a-Flight">
+<table>
+
+	<tr>
+		<td><div class="button_back" id="book_flight" style="background-color: #5DADE2;">
+		
+		<span class="styleText">BOOK A FLIGHT</span>
+		</div></td>
+		<td><div class="button_back" id="reserve" style="background-color: #117864">
+			<span class="styleText">RESERVATION</span>
+		</div></td>
+	</tr>
+	<tr>
+		<td><div class="button_back" id="view_flight"  style="background-color: #D4AC0D">
+			<span class="styleText">VIEW FLIGHT</span>
+		</div></td>
+		<td><div class="button_back" id="nothing" style="background-color: #2471A3">
+			<span class="styleText">HOTEL</span>
+		</div></td>
+	</tr>
+</table>
+
+
+
+
 </center>
 
 <br>
