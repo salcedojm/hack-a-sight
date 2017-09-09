b6bcf0c54e1ebc788bd092e1c507fde9c85ecdec
Joseph Villacorta <jvillacorta2@gmail.com>
2017-09-09 17:30:54 +0800

    FIRST UI

---
+++ homepage.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title> Cebu Pacific </title>
+<style>
+div.snr{
+	font-family:Helvetica;
+	font-size:25px;
+	margin-top:-77px;
+}
+div.header1{
+	font-family:Helvetica;
+	font-size:60px;
+}
+a{
+	text-decoration:none;
+	color:blue;
+}
+</style>
+</head>
+<body>
+<img src="logo.jpg" alt="Cebu pacific logo">
+<div class="snr">
+<p align ="right">
+<a href="">Sign in</a> | <a href="">Register</a></font>
+</div>
+<hr>
+<br>
+<center>
+<font style="font-size:60px;">Welcome, What would you like to do?</font>
+<br>
+<br>
+<br>
+<input type="submit" name="btnBAF" value="Book-a-Flight" width="1000em">
+<input type="submit" name="btnreserve" value="Reservation">
+<br>
+<input type="submit" name="btnvf" value="View Flight">
+<input type="submit" name="btnewan" value="Book-a-Flight">
+</center>
+
+<br>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<title> Cebu Pacific </title>
+<style>
+div.snr{
+	font-family:Helvetica;
+	font-size:25px;
+	margin-top:-77px;
+}
+div.header1{
+	font-family:Helvetica;
+	font-size:60px;
+}
+a{
+	text-decoration:none;
+	color:blue;
+}
+</style>
+</head>
+<body>
+<img src="logo.jpg" alt="Cebu pacific logo">
+<div class="snr">
+<p align ="right">
+<a href="">Sign in</a> | <a href="">Register</a></font>
+</div>
+<hr>
+<br>
+<br>
+<div class="header1">
+Login Info
+</div>
+<hr>
+<br>Email: <input type="text" name="txtemail" placeholder="Email Address">
+<br>Password: <input type="password" name="txtpass" placeholder="Password">
+<br>
+<br>
+<hr>
+<div class="header1">
+Personal Info
+</div>
+<hr>
+<br>Name: 
+<input type="text" name="txtlname" placeholder="Family Name"> 
+<input type="text" name="txtfname" placeholder="Given Name">
+<br>Date of Birth: 
+<input type="text" name="txtday" placeholder="Day"> 
+<input type="text" name="txtmonth" placeholder="Month">
+<input type="text" name="txtyear" placeholder="Year">
+<br>Contact No. <input type="number" name="txtnum" placeholder="Mobile Number">
+<br>
+<br>
+<hr>
+</body>
+</html>
