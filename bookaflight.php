b4e3365656f1cac2024364681c4b71d35861b721
PatriceAnn <patrice.ann0104@gmail.com>
2017-09-10 10:10:16 +0800

    layout

--- bookaflight.php
+++ bookaflight.php
@@ -69,7 +69,7 @@ a{
 <center>
 
 
-<div>
+<div id="round_trip">
 	<form>
 
 		<table>
@@ -123,15 +123,173 @@ a{
 		<center><input type="submit" value="Book now"></center>
 	</form>
 </div>
-
 </div>
-
 </form>
-</div>
+</div></div>
 </center>
-
 </div>
 <br>
+<center><div id="tabs">
+	<table>
+	<span style="font-family: helvetica; font-size: 26px; color: #1B2631">Where would you like to go?</span>
+	<hr>
+		<tr>
+			<td><button class="tab1">Round-trip</button></td>
+			<td> </td>
+			<td><button class="tab1">One-way</button></td>
+			<td> </td>
+			<td><button class="tab1">Multi-city</button></td>
+		</tr>
+
+	</table>
+
+</div></center>
+<div id="booking">
+
+<br>
+<br><br><br>
+<center>
+
+
+<div id="one_way">
+	<form>
+
+		<table>
+			<tr>
+				<td><label>From: </label></td>	
+				<td><select class="combo">
+					<option value ="Philippines">Philippines</option>
+					<option value ="Hong Kong">Hong Kong</option>
+					<option value ="Japan">Japan</option>
+					<option value ="South Korea">South Korea
+					</option>
+					</select></td>
+					<td><label>Departure:</label></td>
+				<td>
+				<input type="text" class="departure" placeholder="Day" name="txtd" value=""> <input type="text" class="departure" placeholder="Month" name="txtm" value=""> <input type="text" class="departure" placeholder="Year" name="txty" value=""></td>
+			</tr>
+
+			<tr>
+				<td><label>To: </label></td>
+				<td><select class="combo">
+					<option value ="Philippines">Philippines</option>
+					<option value ="Hong Kong">Hong Kong</option>
+					<option value ="Japan">Japan</option>
+					<option value ="South Korea">South Korea</option>
+					</select></td>
+				
+			</tr>
+		</table>
+<br>
+
+
+
+		<table>
+		<hr>
+		<span style="font-family: Helvetica; font-size: 20px; color: #2C3E50; font-weight: bold;">AGE</span>
+		<hr>
+			<tr>
+				<td><label>Adult</label></td>
+				<td><label>Child</label></td>
+				<td><label>Infant</label></td>
+			</tr>
+			<tr>
+				<td><input type="text" value=""></div></td>
+				<td><input type="text" value=""></div></td>
+				<td><input type="text" value=""></div></td>
+			</tr>
+		</table>
+		<br>
+		<center><input type="submit" value="Book now"></center>
+	</form>
+</div>
+</div>
+</form>
+</div></div>
+</center>
+</div>
+<br>
+<center><div id="tabs">
+	<table>
+	<span style="font-family: helvetica; font-size: 26px; color: #1B2631">Where would you like to go?</span>
+	<hr>
+		<tr>
+			<td><button class="tab1">Round-trip</button></td>
+			<td> </td>
+			<td><button class="tab1">One-way</button></td>
+			<td> </td>
+			<td><button class="tab1">Multi-city</button></td>
+		</tr>
+
+	</table>
+
+</div></center>
+<div id="booking">
+
+<br>
+<br><br><br>
+<center>
+
+
+<div id="multi_city">
+	<form>
+
+		<table>
+			<tr>
+				<td><label>From: </label></td>	
+				<td><select class="combo">
+					<option value ="Philippines">Philippines</option>
+					<option value ="Hong Kong">Hong Kong</option>
+					<option value ="Japan">Japan</option>
+					<option value ="South Korea">South Korea
+					</option>
+					</select></td>
+					<td><label>Departure:</label></td>
+				<td>
+				<input type="text" class="departure" placeholder="Day" name="txtd" value=""> <input type="text" class="departure" placeholder="Month" name="txtm" value=""> <input type="text" class="departure" placeholder="Year" name="txty" value=""></td>
+			</tr>
+
+			<tr>
+				<td><label>To: </label></td>
+				<td><select class="combo">
+					<option value ="Philippines">Philippines</option>
+					<option value ="Hong Kong">Hong Kong</option>
+					<option value ="Japan">Japan</option>
+					<option value ="South Korea">South Korea</option>
+					</select></td>
+					<td><label>Return:</label></td>
+				<td>
+				<input type="text" class="departure" placeholder="Day" name="txtd1" value=""> <input type="text" class="departure" placeholder="Month" name="txtm1" value=""> <input type="text" class="departure" placeholder="Year" name="txty1" value=""></td>
+			</tr>
+		</table>
+<br>
+
+
+
+		<table>
+		<hr>
+		<span style="font-family: Helvetica; font-size: 20px; color: #2C3E50; font-weight: bold;">AGE</span>
+		<hr>
+			<tr>
+				<td><label>Adult</label></td>
+				<td><label>Child</label></td>
+				<td><label>Infant</label></td>
+			</tr>
+			<tr>
+				<td><input type="text" value=""></div></td>
+				<td><input type="text" value=""></div></td>
+				<td><input type="text" value=""></div></td>
+			</tr>
+		</table>
+		<br>
+		<center><input type="submit" value="Book now"></center>
+	</form>
+</div>
+</div>
+</form>
+</div></div>
+</center>
+</div>
 <hr>
 <footer>
 <img src="logofoot.jpg" alt="credits cebu pacific" height="50" style="margin-right:340px;">© Copyright 2016 cebupacificair.com <img src="visa.jpg" alt="credits visa" style="margin-left:320px;"><img src="mastercard.jpg" alt="credits mastercard"><img src="mastercard-secure.jpg" alt="credits visa"><img src="amex.jpg" alt="credits amex">
